deed714c737ef08590cf3a43d4f1924e9d20304b
jirenmaa <jirenmaa@gmail.com>
2021-02-26 22:25:41 +0700

    refactor(views--home): use localStorage to get the user-data and adding new props to header component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,19 @@
 <template>
   <div class="wrapper">
-    <Header :user="state.user" />
+    <Header :user="state.user" :inUserView="state.inUserView" />
     <Tweat
+      :reciveData="state.reciveData"
       :user="state.user"
       :tweats="state.tweats"
-      @emitFetchData="fetchTweats"
+      @emitFetchData="updateTweats"
     />
     <Sidenav />
   </div>
 </template>
 
 <script>
-// import { users as tweats } from "../assets/users";
-
 import axios from "axios";
-import { useStore } from "vuex";
-import { reactive, computed } from "vue";
+import { reactive } from "vue";
 
 import Tweat from "./Tweat";
 import Header from "@/components/home/Header";
@@ -29,25 +27,31 @@ export default {
     Sidenav,
   },
   setup() {
-    const store = useStore();
-    const tUser = computed(() => store.state.User.user);
+    // const store = useStore();
+    // console.log(store.state.User)
+    // const tUser = computed(() => store.state.User.user);
 
     const state = reactive({
       tweats: [],
-      user: tUser.value,
+      tweats_length: 0,
+      inUserView: false,
+      user: {
+        "username": localStorage.getItem("username"),
+        "avatar": localStorage.getItem("avatar"),
+      },
+      reciveData: false,
     });
 
-    // function refreshFetchData(tweat) {
-    //   console.log(tweat);
-    //   state.tweats.unshift({
-    //     id: state.tweats.length + 1,
-    //     author: state.user.username,
-    //     tweat: tweat,
-    //     images: null,
-    //     likes: null,
-    //     comments: null,
-    //   });
-    // }
+    async function updateTweats(emit) {
+      state.tweats.unshift({
+        id: emit.id,
+        author__username: emit.author__username,
+        author__avatar_url: emit.author__avatar_url,
+        tweat: emit.tweat,
+        picture_url: emit.picture_url,
+        created_at: emit.created_at,
+      })
+    }
 
     async function fetchTweats() {
       const userTweats = await axios({
@@ -60,16 +64,26 @@ export default {
       }).catch((err) => console.error(err))
 
       state.tweats = userTweats.data.data;
+      state.reciveData = true;
+    }
+
+    async function calcTweatsLength() {
+      return state.tweats_length.length
     }
 
     return {
       state,
       fetchTweats,
+      calcTweatsLength,
+      updateTweats,
     };
   },
   async created() {
     await this.fetchTweats();
   },
+  async mounted() {
+    await this.calcTweatsLength();
+  }
 };
 </script>
 
