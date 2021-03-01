1b6bd6a1a598bb5e79ea8d532b8da9978b1cb595
jirenmaa <jirenmaa@gmail.com>
2021-03-01 20:12:11 +0700

    change from development api to production api

--- src/plugin/axios.js
+++ src/plugin/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const baseURL = 'http://localhost:8000/api/';
+const baseURL = 'https://tweatter-api.herokuapp.com/api/';
 const axiosInstance = axios.create({
   baseURL: baseURL,
   headers: {
