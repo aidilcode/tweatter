257413cd81d0ffb699a983d720274e08bc2e0359
jirenmaa <jirenmaa@gmail.com>
2021-04-16 05:12:37 +0700

    change position of comment box

--- src/components/tweats/TweatDetail.vue
+++ src/components/tweats/TweatDetail.vue
@@ -1,16 +1,9 @@
 <template>
   <section>
-    <Comment
-      v-if="commenting"
-      :id="tweatId"
-      :author="repliedTo"
-      :tweat="tweatContent"
-      @closeComment="closedComment"
-    />
     <div class="section-top">
       <div class="top-return">
         <span>
-          <router-link to="/">
+          <router-link :to="prevRoute">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -35,40 +28,42 @@
       </div>
       <div class="top-left"><h2>T</h2></div>
     </div>
-    <div class="tweatters-wrapper">
-      <div class="tweat" v-for="tweat in state.tweats" :key="tweat.id">
+    <div class="tweatters-wrapper" v-if="state.tweats">
+      <div class="tweat">
         <div class="tweat-link">
           <div class="tweat-info">
             <router-link
               :to="{
                 name: 'UserProfile',
-                params: { username: tweat.author.username },
+                params: { username: state.tweats.author.username },
               }"
             >
               <img
-                :src="tweat.author.avatar_url"
+                :src="state.tweats.author.avatar_url"
                 alt=""
                 width="30"
                 height="30"
               />
-              <span class="tweat-username">{{ tweat.author.username }}</span>
+              <span class="tweat-username">{{
+                state.tweats.author.username
+              }}</span>
             </router-link>
             <object
-              :id="tweat.id"
-              @click="moreOption($event, tweat.id)"
+              :id="state.tweats.id"
+              @click="moreOption($event, state.tweats.id)"
               class="dropdown"
               style="float: right"
             >
-              <button class="dropbtn" :id="'dropbtn-' + tweat.id">
+              <button class="dropbtn" :id="'dropbtn-' + state.tweats.id">
                 <FeatherMoreHorizontal />
               </button>
-              <div class="dropdown-content" :id="'more-' + tweat.id">
+              <div class="dropdown-content" :id="'more-' + state.tweats.id">
                 <div>archive</div>
                 <div>report</div>
                 <div
                   class="delete-tweat"
-                  v-if="tweat.author.username == state.user.username"
-                  @click="deleteTweat(tweat.id)"
+                  v-if="state.tweats.author.username == state.user.username"
+                  @click="deleteTweat(state.tweats.id)"
                 >
                   delete
                 </div>
@@ -77,38 +72,32 @@
           </div>
           <div class="tweat-content">
             <div>
-              {{ tweat.tweat }}
+              {{ state.tweats.tweat }}
             </div>
-            <div v-if="tweat.picture_url" class="image">
-              <img :src="tweat.picture_url" alt="" srcset="" />
+            <div v-if="state.tweats.picture_url" class="image">
+              <img :src="state.tweats.picture_url" alt="" srcset="" />
             </div>
           </div>
           <div class="tweat-repr">
             <div class="tweat-comment">
-              <span
-                ><FeatherComments
-                  @click="
-                    commentTweat(
-                      $event,
-                      tweat.author.username,
-                      tweat.tweat,
-                      tweat.id
-                    )
-                  "
-              /></span>
-              <div :id="'comment-' + tweat.id">
-                {{ format(tweat.comments_count) }}
+              <span>
+                <FeatherComments />
+              </span>
+              <div :id="'comment-' + state.tweats.id">
+                {{ format(state.tweats.comments_count) }}
               </div>
             </div>
             <div class="tweat-like">
               <span
-                :id="'isliked-' + tweat.id"
-                :class="{ liked: tweat.likes.includes(state.user.username) }"
-                @click="likeTweat($event, tweat.id)"
+                :id="'isliked-' + state.tweats.id"
+                :class="{
+                  liked: state.tweats.likes.includes(state.user.username),
+                }"
+                @click="likeTweat($event, state.tweats.id)"
                 ><FeatherHeart
               /></span>
-              <div :id="'like-' + tweat.id">
-                {{ format(tweat.likes_count) }}
+              <div :id="'like-' + state.tweats.id">
+                {{ format(state.tweats.likes_count) }}
               </div>
             </div>
             <div class="tweat-share">
@@ -121,11 +110,23 @@
             </div>
           </div>
         </div>
-        <div v-if="tweat.comments.length > 0">
+
+        <div class="user-comment-detail">
+          <div class="wraper-comment">
+            <input
+              type="text"
+              placeholder="Comment your replies"
+              id="content-comment"
+            />
+            <button type="submit" @click="sendComment">send</button>
+          </div>
+        </div>
+
+        <div v-if="state.tweats.comments_count > 0">
           <div class="user-replies">
             <div
               class="comment"
-              v-for="comment in tweat.comments"
+              v-for="comment in state.tweats.comments"
               :key="comment.id"
             >
               <div class="comment-info">
@@ -148,7 +149,7 @@
                     class="username"
                     >{{ comment.users__username }}</router-link
                   >
-                  <small>replying to @{{ tweat.author.username }}</small>
+                  <small>replying to @{{ state.tweats.author.username }}</small>
                 </p>
                 <p class="content">{{ comment.content }}</p>
               </div>
@@ -188,8 +189,6 @@ import FeatherShare from "@/components/icons/FeatherShare";
 import FeatherHash from "@/components/icons/FeatherHash";
 import FeatherMoreHorizontal from "@/components/icons/FeatherMoreHorizontal";
 
-import Comment from "@/components/Comment";
-
 export default {
   name: "TweatDetail",
   components: {
@@ -198,7 +197,16 @@ export default {
     FeatherShare,
     FeatherHash,
     FeatherMoreHorizontal,
-    Comment,
+  },
+  data() {
+    return {
+      commenting: false,
+      repliedTo: "",
+      tweatContent: "",
+      tweatId: "",
+      counts: 0,
+      prevRoute: "/",
+    };
   },
   setup() {
     const route = useRoute();
@@ -212,12 +220,15 @@ export default {
       },
       route: route.path.split("/").slice(2).join("/").replace(/\s/g, ""),
       access: localStorage.getItem("access_token"),
-      tweats: [],
+      tweats: "",
       loading: false,
     });
 
-    async function fetchDetailTweats() {
-      state.loading = true;
+    async function fetchDetailTweats(commented = false) {
+      if (!commented) {
+        state.loading = true;
+      }
+
       await axiosInstance({
         method: "GET",
         url: state.route,
@@ -227,14 +238,41 @@ export default {
         },
       })
         .then((res) => {
-          console.log(res);
-          state.tweats = res.data.results;
+          state.tweats = res.data.results[0];
           state.loading = false;
+          if (commented) {
+            window.scrollTo(0, window.body.scrollHeight);
+          }
         })
         .catch(() => {
           state.loading = false;
           state.status.mssg = "Something is wrong, try to refresh your page.";
         });
+    }
+
+    async function sendComment() {
+      var input = document.getElementById("content-comment");
+      input.classList.toggle("sendings");
+
+      let formData = new FormData();
+      formData.append("content", input.value);
+
+      axiosInstance({
+        method: "POST",
+        url: "tweat/comment/" + state.tweats.id,
+        data: formData,
+        headers: {
+          Authorization: `Bearer ${state.access}`,
+          "Content-Type": "application/json;charset=UTF-8",
+        },
+      })
+        .then(() => {
+          input.classList.toggle("sendings");
+          document.getElementById("content-comment").value = "";
+
+          this.fetchDetailTweats(true);
+        })
+        .catch((err) => console.error(err));
     }
 
     async function likeTweat(event, id) {
@@ -279,7 +317,12 @@ export default {
       }).catch(() => alert("Something went wrong when doing the action"));
     }
 
-    return { state, likeTweat, fetchDetailTweats };
+    return {
+      state,
+      likeTweat,
+      fetchDetailTweats,
+      sendComment,
+    };
   },
   async created() {
     await this.fetchDetailTweats();
@@ -316,10 +359,7 @@ export default {
     },
     closedComment(replied) {
       if (replied) {
-        var counts = document.getElementById("comment-" + this.tweatId);
-        console.info(counts.innerText == "");
-        counts.innerText =
-          counts.innerText == "" ? 1 : Number(counts.innerText) + 1;
+        this.fetchDetailTweats(true);
       }
 
       this.commenting = false;
@@ -327,6 +367,11 @@ export default {
       this.tweatContent = "";
       this.tweatId = "";
     },
+  },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      vm.prevRoute = from.fullPath;
+    });
   },
 };
 </script>
@@ -394,6 +439,7 @@ section {
 }
 .tweat {
   margin-top: 1rem;
+  margin-bottom: 2rem;
   cursor: pointer;
   border-top: 1px solid #222;
   transition: 0.2s ease-in-out;
@@ -547,9 +593,44 @@ section {
     transition: 0.2s ease-in-out;
     background-color: rgb(15, 15, 15);
   }
+  .user-comment-detail {
+  width: 100%;
+  top: 3.8625rem;
+  position: sticky;
+  z-index: 999;
+  background: rgb(18, 18, 18);
+  .wraper-comment {
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    // border-top: 1px solid #222;
+    border-bottom: 1px solid #222;
+  }
+  .sendings {
+    border-color: rgba(52, 211, 153, 0.5) !important;
+  }
+  input[type="text"] {
+    width: 100%;
+    border: 1px solid #222;
+    border-radius: 4px;
+    background: rgba(25, 25, 25, 0.5) !important;
+    padding: 0.5rem 1rem 0.5rem 1rem;
+    color: #bbb !important;
+  }
+  button[type="submit"] {
+    margin: 0 1rem 0 1rem;
+    float: right;
+    color: #bbb;
+    padding: 0.4rem 2.25rem 0.5rem 2.25rem;
+    background: rgba(52, 211, 153, 0.5);
+    border-radius: 4px;
+    &:hover {
+      background: rgba(52, 211, 153, 0.7);
+    }
+  }
+}
 }
 .user-replies {
-  border-top: 1px solid #222;
   .comment {
     padding: 1rem 1.5rem 1rem 1.5rem;
     display: flex;
@@ -577,6 +658,13 @@ section {
       background-color: rgb(20, 20, 20);
       border-top-right-radius: 4px;
       border-bottom-right-radius: 4px;
+    }
+  }
+  .load-more {
+    margin: 1rem 1.5rem 1rem 1.5rem;
+    color: rgba(52, 211, 153, 0.5);
+    &:hover {
+      color: #34d399;
     }
   }
 }
